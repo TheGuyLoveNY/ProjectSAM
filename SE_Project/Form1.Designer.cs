709af8d253b16c9d1d1bff201665bd780b263122
TheGuyLoveNY <kalania@uwindsor.ca>
2018-06-27 02:00:10 +0530

    First Commit

---
+++ SE_Project/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace SE_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.userText = new System.Windows.Forms.RichTextBox();
+            this.samText = new System.Windows.Forms.RichTextBox();
+            this.sam_Label = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.waveBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.waveBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitter1
+            // 
+            this.splitter1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 370);
+            this.splitter1.TabIndex = 2;
+            this.splitter1.TabStop = false;
+            // 
+            // userText
+            // 
+            this.userText.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.userText.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.userText.Font = new System.Drawing.Font("Sitka Display", 10.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userText.ForeColor = System.Drawing.Color.Black;
+            this.userText.Location = new System.Drawing.Point(519, 284);
+            this.userText.Name = "userText";
+            this.userText.Size = new System.Drawing.Size(470, 78);
+            this.userText.TabIndex = 3;
+            this.userText.Text = "Your Command:";
+            this.userText.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.userText.MouseDown += new System.Windows.Forms.MouseEventHandler(this.clearText);
+            // 
+            // samText
+            // 
+            this.samText.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.samText.Font = new System.Drawing.Font("Sitka Display", 10.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.samText.ForeColor = System.Drawing.Color.Black;
+            this.samText.Location = new System.Drawing.Point(810, 19);
+            this.samText.Name = "samText";
+            this.samText.ReadOnly = true;
+            this.samText.Size = new System.Drawing.Size(179, 259);
+            this.samText.TabIndex = 7;
+            this.samText.Text = "SAM\'s response:";
+            // 
+            // sam_Label
+            // 
+            this.sam_Label.AutoSize = true;
+            this.sam_Label.Font = new System.Drawing.Font("OCR A Extended", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sam_Label.ForeColor = System.Drawing.Color.Aqua;
+            this.sam_Label.Location = new System.Drawing.Point(15, 12);
+            this.sam_Label.Name = "sam_Label";
+            this.sam_Label.Size = new System.Drawing.Size(78, 35);
+            this.sam_Label.TabIndex = 8;
+            this.sam_Label.Text = "SAM";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(452, 300);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 62);
+            this.button2.TabIndex = 6;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.BackgroundImage = global::SE_Project.Properties.Resources.Mic;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.button1.Location = new System.Drawing.Point(21, 293);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 62);
+            this.button1.TabIndex = 5;
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mouseDownEvent);
+            this.button1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.mouseUpEvent);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Image = global::SE_Project.Properties.Resources.UI_giff;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 350);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // waveBox
+            // 
+            this.waveBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("waveBox.BackgroundImage")));
+            this.waveBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.waveBox.Image = ((System.Drawing.Image)(resources.GetObject("waveBox.Image")));
+            this.waveBox.Location = new System.Drawing.Point(519, 13);
+            this.waveBox.Name = "waveBox";
+            this.waveBox.Size = new System.Drawing.Size(285, 265);
+            this.waveBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.waveBox.TabIndex = 9;
+            this.waveBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1000, 370);
+            this.Controls.Add(this.waveBox);
+            this.Controls.Add(this.sam_Label);
+            this.Controls.Add(this.samText);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.userText);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.waveBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.RichTextBox userText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox samText;
+        private System.Windows.Forms.Label sam_Label;
+        private System.Windows.Forms.PictureBox waveBox;
+    }
+}
+
---
+++ SE_Project/Form1.cs
@@ -0,0 +1,472 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+using NAudio.Wave;     
+using System.IO;           
+using Google.Cloud.Speech.V1;
+using System.Diagnostics;
+
+
+namespace SE_Project
+{
+    public partial class Form1 : Form
+    {
+
+        // ** Expert System **
+        public string query = "";               //What the user is asking for?
+        public Image thePic = null;
+
+        //** Google speech **//
+        private BufferedWaveProvider bwp;
+
+        WaveIn waveIn;
+        WaveOut waveOut;
+        WaveFileWriter writer;
+        //WaveFileReader reader;
+        string output = "audio.raw";
+
+
+
+        //** Text to speech **//
+        SpeechSynthesizer theSpeaker = new SpeechSynthesizer();             //The actual speaker
+        PromptBuilder pb = new PromptBuilder();                                 //PromptBuilder for more features to the speaker
+        string textPhrase = "";                                                     //THis string is what the speaker will speak.
+
+
+        // ********* List of commands ****************
+        public String[] greetings = { "Hello", "Hi", "Hey", "What Can I do for you?", "How can I help?", "Need help?" , "How can I help you?"};
+
+        public String[] approves = {"Sure!", "You Got it!", "There you go!", "Working on it!","Done!","Okay!","Alright!"};
+
+        public String[] farewell = { "Bye!", "Bye now!", "Farewell!", "See you!", "See you soon!", "Adios!" };
+
+        public Form1()
+        {
+            InitializeComponent();
+            thePic = waveBox.Image;
+
+
+            if (CheckForInternetConnection())
+            {
+                //Internet works. SAM greets.
+                samText.Text = "";
+                Say(greetings[randomChoice(greetings)]);
+            }
+
+            else
+                Say("Your internet is not connected. I may not fully be able to help you!");
+
+
+            theSpeaker.Volume = 100;
+            theSpeaker.Rate = -1;
+
+            
+            waveBox.Image = null;
+            // ********** Google Cloud Speech ****************
+
+            waveOut = new WaveOut();
+            waveIn = new WaveIn();
+
+
+            //configuring the settings for the audio file
+            waveIn.DataAvailable += new EventHandler<WaveInEventArgs>(waveIn_DataAvailable);
+            waveIn.WaveFormat = new NAudio.Wave.WaveFormat(16000, 1);           //16000hz
+            bwp = new BufferedWaveProvider(waveIn.WaveFormat);
+            bwp.DiscardOnBufferOverflow = true;                         //Discard data if overflowed.
+
+            if (NAudio.Wave.WaveIn.DeviceCount < 1)
+            {
+                Say("No microphone!");
+                return;
+            }
+
+
+
+        }
+
+
+        //Event handler for recording voice
+        void waveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            bwp.AddSamples(e.Buffer, 0, e.BytesRecorded);
+
+        }
+
+
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Expert(); //Speaks
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mouseDownEvent(object sender, MouseEventArgs e)
+        {
+            //When the mouse is pressed... Listen to the audio
+            waveIn.StartRecording();
+        }
+
+        private void mouseUpEvent(object sender, MouseEventArgs e)
+        {
+
+            //****************** Saving the speech into a raw file ********************************
+            //When the mouse is released
+            //stop recodring and save the file (Overwrtie if exist)
+            waveIn.StopRecording();
+
+            if (File.Exists("audio.raw"))
+                File.Delete("audio.raw");
+
+            //Create the raw audio file, with the given hz
+            writer = new WaveFileWriter(output, waveIn.WaveFormat);
+
+            byte[] buffer = new byte[bwp.BufferLength];
+            int offset = 0;
+            int count = bwp.BufferLength;
+
+            var read = bwp.Read(buffer, offset, count);
+            if (count > 0)
+            {
+                writer.Write(buffer, offset, read);
+            }
+
+            waveIn.Dispose();
+            waveIn = null;
+            writer.Close();
+            writer = null;
+
+            // ***************** RETRY
+
+            waveOut = new WaveOut();
+            waveIn = new WaveIn();
+
+            
+            //configuring the settings for the audio file
+            waveIn.DataAvailable += new EventHandler<WaveInEventArgs>(waveIn_DataAvailable);
+            waveIn.WaveFormat = new NAudio.Wave.WaveFormat(16000, 1);           //16000hz
+            bwp = new BufferedWaveProvider(waveIn.WaveFormat);
+            bwp.DiscardOnBufferOverflow = true;                         //Discard data if overflowed.
+
+            // ***************************** speech to text conversion*******************************
+
+            if (File.Exists("audio.raw"))
+            {
+                //create a speech client, (Google cloud speech API)
+                var speech = SpeechClient.Create();
+
+                //Start recognizing the audio
+                var response = speech.Recognize(new RecognitionConfig()
+                {
+                    Encoding = RecognitionConfig.Types.AudioEncoding.Linear16,
+                    SampleRateHertz = 16000,
+                    LanguageCode = "en",
+                }, RecognitionAudio.FromFile("audio.raw"));
+
+                //Initializing the text 
+                userText.Text = "";
+
+
+                //Get each response in text
+                foreach (var result in response.Results)
+                {
+                    foreach (var alternative in result.Alternatives)
+                    {
+                        userText.Text = alternative.Transcript + userText.Text;
+                    }
+                }
+
+               
+
+                //if nothing was said in the audio
+                if (userText.Text.Length == 0)
+                    userText.Text = "No Data ";
+
+            }
+            else
+            {
+                //if the audio file was not found
+                userText.Text = "Audio File Missing ";
+
+            }
+
+
+            this.Expert(); 
+
+        }
+
+
+
+        // *********************** EXPERT SYSTEM ***************************
+
+            // NOTE : Treat all string as a lower case.
+
+        private void Expert()
+        {
+            query = userText.Text.ToLower();
+
+            //Facebook
+            if (query == "hi" || query == "hey" || query ==  "hello" || query == "how are you" || query == "what is up" || query == "how you doing")
+            {
+                Say(greetings[randomChoice(greetings)]);
+            }
+
+            else if (query.Contains("who are you") || query == "who is this" || query.Contains("what can you do") || query == "what are you" || query.Contains("how can you help me") || query.Contains("how can i use you") || query.Contains("what can you do for me") || query.Contains("introduce yourself") || query.Contains("who is sam"))
+            {
+                Say("Hello, My developers named me Sam, a System Assisted Manager. I can help you with anything. I can automate your system, open programs, tell you weather, time, news and much more. I am here to help, Just ask!");
+            }
+
+            else if (query.Contains("open word") || query == "microsoft word" || query.Contains("open ms word") || query.Contains("write a word document") || query.Contains("open microsoft word"))
+            {
+                Say(approves[randomChoice(approves)]);
+                Process.Start(@"C:\Program Files\Microsoft Office\Office16\WINWORD.exe");
+            }
+
+            else if (query.Contains("open music player") || query == "music player" || query.Contains("open media player") || query.Contains("play a music") || query.Contains("play a song") || query.Contains("play music"))
+            {
+                Say(approves[randomChoice(approves)]);
+                Say("Please choose a song.");
+
+                Process.Start(@"C:\Program Files (x86)\Windows Media Player\wmplayer.exe");
+            }
+
+
+
+            else if (query.Contains("open browser") || query.Contains("browser") || query.Contains("open internet") || query.Contains("internet") || query.Contains("open web") || query.Contains("web"))
+            {
+                Say(approves[randomChoice(approves)]);
+                Process.Start("https://www.google.com");
+            }
+
+            else if (query.Contains("what is the time")|| query.Contains("what time is it") || query.Contains("time now"))
+            {
+                Say("The time is " + DateTime.Now.ToString("h:mm tt"));
+            }
+
+            else if (query.Contains("what is the day") || query.Contains("what day is it") || query.Contains("date") || query.Contains("what is today"))
+            {
+                Say("Today is " + DateTime.Now.ToString("M/d/yyyy"));
+            }
+
+            else if (query.Contains("open facebook") || query.Contains("facebook") || query.Contains("fb") || query.Contains("face book"))
+            {
+                Say(approves[randomChoice(approves)]);
+                Process.Start("https://www.facebook.com/");
+            }
+
+            //Youtube
+            else if (query.Contains("open youtube") || query.Contains("youtube") || query.Contains("yt") || query.Contains("you tube"))
+            {
+                Say(approves[randomChoice(approves)]);
+                Process.Start("https://www.youtube.com/");
+            }
+
+
+            //Twitter
+            else if (query.Contains("open twitter") || query.Contains("twitter"))
+            {
+                Say(approves[randomChoice(approves)]);
+                Process.Start("https://twitter.com/");
+            }
+
+            //IG
+            else if (query.Contains("open instagram") || query.Contains("instagram") || query.Contains("ig"))
+            {
+                Say(approves[randomChoice(approves)]);
+                Process.Start("https://www.instagram.com/");
+            }
+
+            //Google
+            else if (query.Contains("open google") || query.Contains("google"))
+            {
+                Say(approves[randomChoice(approves)]);
+                Process.Start("https://www.google.com");
+            }
+
+            else if (query.Contains("hide") || query.Contains("minimize"))
+            {
+                if (this.WindowState == FormWindowState.Normal)
+                {
+                    Say(approves[randomChoice(approves)]);
+                    this.WindowState = FormWindowState.Minimized;
+                }
+            }
+
+            else if (query.Contains("maximize") || query.Contains("show up") || query.Contains("where are you"))
+            {
+                if (this.WindowState == FormWindowState.Minimized)
+                {
+                    Say("Here I am.");
+                    this.WindowState = FormWindowState.Normal;
+                }
+            }
+            else if (query.Contains("thank you"))
+            {
+                Say("My Pleasure!");
+
+            }
+
+            else if (query.Contains("who is pooya"))
+            {
+                Say("He is a Professor at Windsor University. Very generous at grading students. Does not likes to be called professor or doctor, Just Pooya!");
+
+            }
+
+            //Less imp
+            else if (query.Contains("what") || query.Contains("who") || query.Contains("which") || query.Contains("how") || query.Contains("why") || query.Contains("when"))
+            {
+
+                Say(approves[randomChoice(approves)]);
+                Process.Start("https://www.google.ca/search?q=" + query);
+            }
+
+            // Least important
+            else if (query.Contains("sam"))
+            {
+                
+                if (query.Contains("turn off") || query.Contains("bye") || query.Contains("shutdown") || query.Contains("shut down") || query.Contains("goodbye"))
+                {
+                    Say(farewell[randomChoice(farewell)]);
+                    Application.Exit();
+                }
+            }
+
+            //When all else fails
+            else
+            {
+                Say("let me find it for you");
+                Process.Start("https://www.google.ca/search?q=" + query);
+            }
+
+            //Save the report
+            saveLogFile();
+
+        }
+
+
+
+        // ***************** SAM SPeaks *********************
+        private void Say(string speakString)
+        {
+            waveBox.Image = thePic;
+            textPhrase = "";
+
+            textPhrase = speakString;
+            pb.AppendText(textPhrase, PromptRate.Medium);
+            theSpeaker.SpeakAsync(pb);
+
+
+            samText.Text += textPhrase + "\n";
+            pb.ClearContent();
+            theSpeaker.SpeakCompleted += new EventHandler<SpeakCompletedEventArgs>(voiceComplete);
+  
+        }
+
+        private void voiceComplete(object sender, EventArgs e)
+        {
+            //When the speak is complete
+            waveBox.Image = null;
+        }
+
+        //*************** Random string generator *******************
+        private int randomChoice(String[] theList)
+        {
+         
+            Random r = new Random();
+
+            //Random number from 0 to the length of the list
+            return (r.Next(0, theList.Length));
+        }
+
+        public static bool CheckForInternetConnection()
+        {
+            try
+            {
+                using (var client = new System.Net.WebClient())
+                {
+                    using (client.OpenRead("http://clients3.google.com/generate_204"))
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void clearText(object sender, MouseEventArgs e)
+        {
+            userText.Text = "";
+        }
+
+
+        public static void killProcess(String s)
+        {
+            System.Diagnostics.Process[] procs = null;
+            try
+            {
+                procs = Process.GetProcessesByName(s);
+                Process curProg = procs[0];
+
+                if (!curProg.HasExited)         //kill the main process
+                    curProg.Kill();
+            }
+            finally
+            {   //if child processes are still alive, kill them all
+                if (procs != null)
+                    foreach (Process p in procs)
+                        p.Dispose();
+            }
+        }
+
+
+
+        private void saveLogFile()
+        {
+            string path = @"C:\ProgramData\SAM_logFile.txt";
+            string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;  //gets the user name
+
+            //if the file does not exists.
+            if (!File.Exists(path))
+            {
+                File.Create(path).Dispose();
+                using (TextWriter tw = new StreamWriter(path))
+                {
+                    tw.WriteLine("*** Log File ****");
+                    tw.Close();
+                }
+
+            }
+
+            //keep writing 
+            else if (File.Exists(path))
+            {
+                using (TextWriter tw = new StreamWriter(path,true)) //Parameter: path, append : bool
+                {
+                    tw.WriteLine("Name: "+userName+"\t"+ "Command: " + query +"\t"+ "Time: " + DateTime.Now.ToString("h:mm tt")+"\t"+ "Day: " + DateTime.Now.ToString("M/d/yyyy"));
+                    tw.Close();
+                }
+            }
+        }
+    }
+}
+
